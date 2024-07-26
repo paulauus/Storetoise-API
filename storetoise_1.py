3d0bbf6d6817ec3ad11d438092b87da1461faf97
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-07-26 13:51:08 +0100

    Initial commit

---
+++ storetoise_1.py
@@ -0,0 +1,10 @@
+"""The Storetoise CLI."""
+
+from argparse import ArgumentParser
+import requests
+
+BASE_URL = "http://storetoise-lb-1365832025.eu-west-2.elb.amazonaws.com"
+
+if __name__ == "__main__":
+
+  pass
