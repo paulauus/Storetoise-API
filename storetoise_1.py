e02de17e1be3d472b06a1ce9f707219b4f404031
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-07-26 14:17:15 +0100

    CLI input

--- storetoise_1.py
+++ storetoise_1.py
@@ -5,6 +5,16 @@ import requests
 
 BASE_URL = "http://storetoise-lb-1365832025.eu-west-2.elb.amazonaws.com"
 
+def load_storetoise_data(url: str) -> dict:
+  ...
+
+def command_line_input():
+  parser = ArgumentParser()
+  parser.add_argument("--username", "-u", required=True)
+  args = parser.parse_args()
+  return args
+  ...
+
 if __name__ == "__main__":
 
   pass
