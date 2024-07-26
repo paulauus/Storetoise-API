ee07b6723605a07b9cca7ea9b9c844fe498c001e
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-07-26 14:22:33 +0100

    Load data

--- storetoise_1.py
+++ storetoise_1.py
@@ -6,9 +6,14 @@ import requests
 BASE_URL = "http://storetoise-lb-1365832025.eu-west-2.elb.amazonaws.com"
 
 def load_storetoise_data(url: str) -> dict:
+  request = requests.get(url, timeout=10)
+  request = request.json()
+  return request
+
+def print_storage_ids(data: dict, number:int) -> None:
   ...
 
-def command_line_input():
+def command_line_interface_input():
   parser = ArgumentParser()
   parser.add_argument("--username", "-u", required=True)
   args = parser.parse_args()
